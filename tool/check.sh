9517a292a6b24aaacbb2478652962ba2443ee422
Radomir Epur <radomir9720@gmail.com>
2023-02-14 20:04:06 +0300

    chore: added check.sh script

---
+++ tool/check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+flutter pub get &&
+dart format --set-exit-if-changed lib test &&
+flutter analyze lib test &&
+flutter test --coverage --test-randomize-ordering-seed random && 
+cd widgetbook_app &&
+flutter pub get &&
+dart format --set-exit-if-changed lib test &&
+flutter analyze lib
